7fc11487a3d8103f5806aa98ce2f53d5fc7190f6
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-09 21:44:09 +0530

    s-5

---
+++ right_aligned_inverted_triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        int i,j,k;
+        for(i=5;i>=1;i=i-1){
+            for(k=0;k<=5-i;k=k+1){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j=j+1){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
