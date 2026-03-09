20adda51796b1188d1934856a50231b988996e5c
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-09 21:44:46 +0530

    s-7

---
+++ star_pattern.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        int i,j;
+        for(i=1;i<=5;i=i+1){
+            for(j=1;j<=i;j=j+1){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
