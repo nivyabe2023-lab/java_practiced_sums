98f8de17694bd04331d3285d66703f9089a1b733
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-10 22:56:47 +0530

    s-9

---
+++ inverted_hallow_star.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++){
+            for(j=1;j<=5;j++){
+
+                if(i==1 || j==1 || j==6-i){
+                    System.out.print("*");
+                } 
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
