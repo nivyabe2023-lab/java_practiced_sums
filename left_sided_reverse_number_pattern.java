f598edd4dba8c0e398729e70b42eba4317e83ecf
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-10 23:01:54 +0530

    s-12

---
+++ left_sided_reverse_number_pattern.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+    int i,j;
+	    for(i=5;i>=1;i=i-1){
+	        for(j=i;j>=1;j=j-1){
+	            System.out.print(j);
+	        }
+	        System.out.println();
+	    }
+	    for(i=2;i<=5;i=i+1){
+	        for(j=i;j>=1;j=j-1){
+	            System.out.print(j);
+	        }
+	        System.out.println();
+	    }
+	    
+	}
+}
